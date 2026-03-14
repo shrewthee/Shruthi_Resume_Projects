6cd60218d5d0795a238f6b1860b5f20d74a55368
shrewthee <111901842+shrewthee@users.noreply.github.com>
2026-03-14 14:22:03 -0400

    Add files via upload
    Budgeting application where user can input their money gains and loses and the application updates the balance according to the inputs

---
+++ script.js
@@ -0,0 +1,58 @@
+// //variables
+// let balance = 2000;
+// const description = "Salary";
+// const newTransaction = true;
+// //const balanceNumberEl = document.querySelector('.balance-number');
+// //balanceNumberEl.textContent = balance;
+// console.log(1000);
+// //use const unless you want to maybe change the value then use let
+
+
+//data types and structures
+// const text = 'Hello';
+// const salary = 2000;
+// const isExpense = false;
+// const expenses = [1000,30,25];
+// console.log(expenses.length);
+// const transaction = {
+//     description: 'Salary',
+//     amount: 5000
+// }
+// transaction.description
+
+
+// //functions
+// function calculateBalance(income){
+//     const expenses = 2000;
+//     const balance = income - expenses;
+//     console.log(balance);
+// }
+// calculateBalance(5000);
+//arrow function
+// const calculateBalance = (income, expenses) => income - expenses; 
+
+
+//interacting with HTML and CSS
+// const headingEl = document.querySelector("first-heading");
+// headingEl.textContent = "blabla";
+// const balanceEl = document.querySelector('.balance-number');
+// balanceEl.addEventListener('click', () => {
+//     const balance = 5000-3000;
+//     console.log(balance);
+// }
+// )
+
+
+//scoping and hoisting
+// if(1000>500){
+//     const amount = 5000;
+// }
+// function addTransaction(){
+//     let amount = 5000;
+//     console.log(amount);
+// }
+
+// addTransaction();
+
+
+//string concatenation
